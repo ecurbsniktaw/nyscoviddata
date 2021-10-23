22063d5b2a4dac444d2bf24ad7ee5da280ce9c87
Bruce Watkins <code@brucewatkins.org>
2021-10-23 08:58:01 -0400

    first commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Header from '$lib/header/Header.svelte';
+	import '../app.css';
+</script>
+
+<Header />
+
+<main>
+	<slot />
+</main>
+
+<footer>
+	<p>put something here (it's the footer)</p>
+</footer>
+
+<style>
+
+</style>
